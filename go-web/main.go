176e8d2ab38f2574e28ba8bf9308e94ae3c4273c
kasheemlew <417879751@qq.com>
2017-03-12 16:32:59 +0800

    add sql

--- go-web/main.go
+++ go-web/main.go
@@ -6,6 +6,12 @@ import (
     "html/template"
     "strings"
     "log"
+    "mime/multipart"
+    "crypto/md5"
+    "time"
+    "io"
+    "strconv"
+    "os"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -24,19 +30,36 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
     fmt.Println("method:", r.Method)
     if r.Method == "GET" {
+        // set token to void duplicate submit
+        crutime := time.Now().Unix()
+        h := md5.New()
+        io.WriteString(h, strconv.FormatInt(crutime, 10))
+        token := fmt.Sprintf("%x", h.Sum(nil))
+        // same as token : fmt.Sprintf("%x", h.Sum(strconv.FormatInt(crutime,
+        // 10)))
+
         t, _ := template.ParseFiles("./templates/login.gtpl")
-        t.Execute(w, nil)
+        t.Execute(w, token)
     } else {
         r.ParseForm()
+        token := r.Form.Get("token")
+
+        // check token
+        if token != "" {
+        } else {
+        }
 
         // r.Form.Getl() for nil
         if len(r.FormValue("username"))==0 {
             fmt.Println("no username!")
         }
-        fmt.Println("username:", r.Form["username"])
+        // fmt.Println("username:", r.Form["username"])
         // use r.FormValue("username") instead of
         // r.ParseForm()+r.Form["username"]
-        fmt.Println("password:", r.Form["password"])
+        // fmt.Println("password:", r.Form["password"])
+        fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+        fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+        template.HTMLEscape(w, []byte(r.Form.Get("username")))
     }
 }
 
@@ -83,10 +106,40 @@ func info(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("method: ", r.Method)
+    if r.Method == "GET" {
+        crutime := time.Now().Unix()
+        h := md5.New()
+        io.WriteString(h, strconv.FormatInt(crutime, 10))
+        token := fmt.Sprintf("%x", h.Sum(nil))
+
+        t, _ := template.ParseFiles("./templates/upload.gtpl")
+        t.Execute(w, token)
+    } else {
+        r.ParseMultipartForm(32 << 20) // max file size
+        file, handler, err := r.FormFile("uploadfile")
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer file.Close()
+        fmt.Fprintf(w, "%v",handler.Header)
+        f, err := os.OpenFile("./uploadfiles/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer f.Close()
+        io.Copy(f, file)
+    }
+}
+
 func main() {
     http.HandleFunc("/", sayhelloName)
     http.HandleFunc("/login", login)
     http.HandleFunc("/info", info)
+    http.HandleFunc("/upload", upload)
     server := "localhost"
     port := "9090"
     fmt.Println("Server", server, " Listening on", port, "...")
--- go-web/templates/login.gtpl
+++ go-web/templates/login.gtpl
@@ -3,9 +3,10 @@
     <title></title>
     </head>
     <body>
-    <form action="/login/" method="post">
+    <form action="/login" method="post">
         username:<input type="text" name="username">
         passowrd:<input type="password" name="password">
+        <input type="hidden" name="token" value="{{.}}">
         <input type="submit" value="login">
     </form>
     </body>
